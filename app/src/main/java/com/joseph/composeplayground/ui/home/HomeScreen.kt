88afddcde0f170d59e189e54249e8dfc2954ddcf
junieberry <junyoung44@g.skku.edu>
2022-02-17 16:15:24 +0900

    [Feat] Home UpcomingMovieList

--- app/src/main/java/com/joseph/composeplayground/ui/home/HomeScreen.kt
+++ app/src/main/java/com/joseph/composeplayground/ui/home/HomeScreen.kt
@@ -1,11 +1,14 @@
 package com.joseph.composeplayground.ui.home
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.rememberScrollableState
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -17,7 +20,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,6 +32,9 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.rememberImagePainter
+import com.joseph.composeplayground.R
+import com.joseph.composeplayground.model.Movie
 import com.joseph.composeplayground.ui.home.dto.HomeAction
 import com.joseph.composeplayground.ui.theme.ComposePlaygroundTheme
 import com.joseph.composeplayground.ui.theme.Suit
@@ -49,6 +58,8 @@ fun HomeScreen(
             Title()
             Spacer(modifier = Modifier.height(16.dp))
             MovieSearch(title = uiState.value.movieSearch, viewModel = viewModel)
+            Spacer(modifier = Modifier.height(16.dp))
+            UpcomingMovies(movies = uiState.value.upComingMovieList.movies)
         }
     }
 }
@@ -56,9 +67,7 @@ fun HomeScreen(
 @Composable
 private fun Title() {
     // 패딩 값 예쁘게 수정
-    Column(
-        modifier = Modifier.padding(bottom = 20.dp)
-    ) {
+    Column {
         Text(
             text = "Compose Playground",
             fontSize = 16.sp,
@@ -82,6 +91,8 @@ private fun MovieSearch(title: String, viewModel: HomeViewModel) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
+            .height(34.dp)
+            .clip(shape = RoundedCornerShape(10.dp)),
     ) {
         // border rounding 처리
         TextField(
@@ -96,21 +107,72 @@ private fun MovieSearch(title: String, viewModel: HomeViewModel) {
 }
 
 @Composable
-private fun UpcomingMovies() {
-
+private fun UpcomingMovies(movies: List<Movie>) {
+    Column() {
+        Text(
+            text = "Upcoming Movies",
+            fontSize = 14.sp,
+            fontFamily = Suit,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        LazyRow() {
+            items(items = movies) { movie ->
+                UpcomingMovie(movie = movie)
+            }
+        }
+    }
 }
 
 @Composable
-private fun UpcomingMove() {
-}
-
-@Composable
-private fun PopularMovies() {
-}
-
-
-@Preview()
-@Composable
-fun forPreview() {
-//    MovieSearch()
+private fun UpcomingMovie(movie: Movie) {
+    Box(
+        modifier = Modifier
+            .size(180.dp, 260.dp)
+            .clip(RoundedCornerShape(5.dp))
+            .padding(horizontal = 4.dp)
+            .background(
+                brush = Brush.verticalGradient(
+                    listOf(
+                        Color.Transparent,
+                        Color.Black.copy(alpha = 0.8f)
+                    ),
+                    0f,
+                    100f,
+                )
+            )
+    ) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://image.tmdb.org/t/p/w500${movie.posterPath}"
+            ),
+            contentDescription = movie.title,
+            contentScale = ContentScale.FillBounds,
+        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 4.dp, end = 4.dp)
+                .wrapContentWidth(),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = movie.title ?: "",
+                fontSize = 16.sp,
+                fontFamily = Suit,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                textAlign = TextAlign.End
+            )
+            Text(
+                text = movie.originalTitle ?: "",
+                fontSize = 12.sp,
+                fontFamily = Suit,
+                fontWeight = FontWeight.Light,
+                color = Color.White,
+                textAlign = TextAlign.End
+            )
+        }
+    }
 }
